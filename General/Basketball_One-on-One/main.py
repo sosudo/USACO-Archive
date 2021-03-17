898e0b6c9bcadcaf1663c13a18cf6a85048441a8
Imperial-Industries <33400784+Imperial-Industries@users.noreply.github.com>
2021-03-17 00:27:29 -0400

    Update main.py

--- General/Basketball_One-on-One/main.py
+++ General/Basketball_One-on-One/main.py
@@ -2,6 +2,9 @@ record = input()
 record_list = [i for i in record]
 A, B, win_by_two, winner = 0, 0, False, None
 while winner == None:
+  if len(record_list) == 0:
+    winner = 'A' if A > B else 'B'
+    break
   scorer = record_list[0]
   record_list.pop(0)
   points = int(record_list[0])
@@ -24,8 +27,10 @@ while winner == None:
   if win_by_two == False:
     if A == 11:
       winner = 'A'
+      break
     elif B == 11:
       winner = 'B'
+      break
     else:
       continue
 print(winner)
