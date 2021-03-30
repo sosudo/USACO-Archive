aa2174a43df51d9ec850831d5c91aa624fa6c700
Imperial-Industries <33400784+Imperial-Industries@users.noreply.github.com>
2021-03-30 16:59:39 -0400

    Update main.py

--- General/Teleportation/main.py
+++ General/Teleportation/main.py
@@ -4,5 +4,4 @@ sys.stdout = open('teleport.out', 'w')
 a, b, x, y = [int(i) for i in input().split(" ")]
 ans_one = (abs(min([a,b])-min([x,y]))+abs(max([a,b])-max([x,y])))
 ans_two = abs(b-a)
-ans_three = abs(a-b)
-print(min([ans_one, ans_two, ans_three]))
+print(min([ans_one, ans_two]))
